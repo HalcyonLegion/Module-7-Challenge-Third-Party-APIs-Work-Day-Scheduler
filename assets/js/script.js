11a2115d080261bcf6f76dad9b7f02695e18d2be
Lee Webb <leewerbb55@hotmail.com>
2023-04-04 14:32:01 +0100

    Updated some of my comments to better reflect the process of learning through this!

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,17 @@
 
 // Display the current day at the top of the calender when a user opens the planner. - moment.js
 
-// Present timeblocks for standard business hours when the user scrolls down. - moment.js/jquery html insertion magic.
+// Present timeblocks for standard business hours when the user scrolls down. - moment.js/jquery html insertion.
 
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed. Some sort of conditional time-based css/moment.js mashup. There are classes in the CSS files for .past .present .future. so this is going to be wild.
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed. Some sort of conditional time-based css/moment.js mashup. There are classes in the CSS files for .past .present .future.
 
-// Allow a user to enter an event when they click a timeblock. Textarea? Yes I can see this in the CSS - great!
+// Allow a user to enter an event when they click a timeblock. Textarea? Yes I can see this in the CSS.
 
-// Save the event in local storage when the save button is clicked in that timeblock. Using localstorage means I need to json stringify somewhere and then remember to parse it.
+// Save the event in local storage when the save button is clicked in that timeblock. Using localstorage I thought meant I needed to json stringify somewhere and then remember to parse it later, turns out I didn't.
 
-// Persist events between refreshes of a page - prevent default? No turns out I can just do it like the below.
+// Persist events between refreshes of a page - prevent default? No, turns out I can just do it like the below.
 
-// Opening the page to start with looks like this entire thing is going to have to be driven by jQuery dynamically creating and appending various elements. The Demo Gif in the Readme seems to only have work hours - so Days are not a factor? Hours are 9-5pm.
+// Opening the page to start with looks like this entire structure is going to have to be driven by jQuery dynamically creating and appending various elements. The Demo Gif in the Readme seems to only have work hours - so Days are not a factor? Hours are 9-5pm.
 
 $(document).ready(function() {
     // Variables - The work hours will need to be arrayed so they can be added to the page like in previous lessons.
