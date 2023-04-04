f54b32ebfb272512b7f37f47cc87a0965fd48102
Lee Webb <leewerbb55@hotmail.com>
2023-04-04 12:00:10 +0100

    Added some jquery bits to create new elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@
 
 // Persist events between refreshes of a page - prevent default.
 
-//Opening the page to start with looks like this entire thing is going to have to be driven by jQuery dynamically creating and appending various elements. The Demo Gif in the Readme seems to only have work hours - so Days are not a factor? Hours are 9-5pm.
+// Opening the page to start with looks like this entire thing is going to have to be driven by jQuery dynamically creating and appending various elements. The Demo Gif in the Readme seems to only have work hours - so Days are not a factor? Hours are 9-5pm.
 
 $(document).ready(function() {
     // Variables - I guess the work hours will need to be arrayed so they can be added to the page like in previous lessons.
@@ -22,5 +22,20 @@ $(document).ready(function() {
 
     // I need to get the current day bit out the way - thankfully the html has a handy ID called currentDay I can grab for this.
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+    // Creating the timeblocks for each work hour. The value for each hour will need to be passed as a variable at some point.
+    workHours.forEach(function(hour) {
+    // Create a row for each timeblock
+    var row = $("<div>").addClass("row time-block");
+
+    // Creating an hour column
+    var hourCol = $("<div>")
+    .addClass("col-md-1 hour")
+    .text(moment(hour, "H").format("h A"));
+
+    // Creating a textarea column to enter the items for the scheduler
+    var textArea = $("<textarea>").addClass("col-md-10");
+    textArea.attr("id", "hour-" + hour);
+
 
 });
