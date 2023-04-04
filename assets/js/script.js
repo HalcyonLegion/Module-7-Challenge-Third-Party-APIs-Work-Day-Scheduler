8ffe5f6757236b57c8837a101374b431cd15affc
Lee Webb <leewerbb55@hotmail.com>
2023-04-04 14:17:08 +0100

    Very minor changes to some earlier comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,11 @@
 // Opening the page to start with looks like this entire thing is going to have to be driven by jQuery dynamically creating and appending various elements. The Demo Gif in the Readme seems to only have work hours - so Days are not a factor? Hours are 9-5pm.
 
 $(document).ready(function() {
-    // Variables - I guess the work hours will need to be arrayed so they can be added to the page like in previous lessons.
+    // Variables - The work hours will need to be arrayed so they can be added to the page like in previous lessons.
     var workHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
     var container = $(".container");
 
-    // I need to get the current day bit out the way - thankfully the html has a handy ID called currentDay I can grab for this.
+    // I need to get the current day bit out the way - thankfully the html has a handy ID called currentDay.
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
     // Creating the timeblocks for each work hour. The value for each hour will need to be passed as a variable at some point.
